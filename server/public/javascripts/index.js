4fe2d1c0e9bd0a71f83e68439ca92432637ab539
Thomas Henderson <tbhenderson55@yahoo.com>
2019-04-27 21:56:29 +0900

    styled form working

--- server/public/javascripts/index.js
+++ server/public/javascripts/index.js
@@ -5,13 +5,13 @@ const inputDescription = document.getElementById("input-description")
 const inputAlcohol = document.getElementById("input-alcohol")
 const inputForm = document.getElementById("input-form")
 const submitBtn = document.getElementById("input-btn")
-console.log(inputName)
+// console.log(inputName)
 
-inputForm.addEventListener("submit", function(e)  {
-  e.preventDefault()
-  console.log('clcked')
-  console.log(input.value)
-})
+// inputForm.addEventListener("submit", function(e)  {
+//   e.preventDefault()
+//   console.log('clcked')
+//   console.log(input.value)
+// })
 // document.addEventListener("click", function()  {
 //   // e.preventDefault()
 //   alert('clicked')
